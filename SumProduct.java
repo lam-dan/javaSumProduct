706ef084017b244b05dbd0ae7f7f6c4e0651edaf
Daniel <danlam@ucdavis.edu>
2021-09-08 21:42:50 -0700

    Update SumProduct.java

--- SumProduct.java
+++ SumProduct.java
@@ -5,22 +5,22 @@ public class SumProduct {
 		String input1;
 		String input2;
 		Scanner terminal = new Scanner(System.in);
-		
+
 		System.out.println("Please enter a number: ");
 		while (!terminal.hasNextInt()) {
 			input1 = terminal.next();
 			System.out.printf("\"%s\" is not a valid number.\n", input1);
 			System.out.println("Please enter a valid number:");
-        }
+		}
 		int num1 = Integer.parseInt(terminal.next());
 		System.out.println("You have entered "+ num1 + ".");
-		
+
 		System.out.println("Please enter another number: ");
 		while (!terminal.hasNextInt()) {
 			input2 = terminal.next();
 			System.out.printf("\"%s\" is not a valid number.\n", input2);
 			System.out.println("Please enter a valid number:");
-        }
+		}
 		int num2 = Integer.parseInt(terminal.next());
 		System.out.println("You have entered "+ num2 + ".");
 		System.out.println("You have entered "+ num1 + " and " + num2 + ".");
@@ -28,6 +28,6 @@ public class SumProduct {
 		int product = num1 * num2;
 		System.out.println("The sum of these numbers are: " + sum + ".");
 		System.out.println("The product of these numbers are: " + product + ".");
-	}
+		}
 }
 	
